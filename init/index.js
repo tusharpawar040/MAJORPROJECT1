7eee9e42c867bc13893aa9aae01980c080ae1744
Tushar Pawar <tp0407755@gmail.com>
2024-01-30 14:13:22 +0530

    Add Project Files

---
+++ init/index.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const initData = require("./data2.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+    .then(() => {
+        console.log("connected to db");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({
+        ...obj,
+        owner: "65af0fc2d7d6ec7e80172fe2",
+    }));
+    await Listing.insertMany(initData.data);
+    console.log("data was initialized");
+}
+
+initDB();
